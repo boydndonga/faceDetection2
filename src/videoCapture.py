5da77729428491189759439e55c0a86e108686e8
boyd ndonga <boydbugle@gmail.com>
2018-10-11 11:57:37 +0300

    create video capture frame

--- src/videoCapture.py
+++ src/videoCapture.py
@@ -1,2 +1,17 @@
 import numpy as np 
 import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    #capture video frame
+    ret, frame = cap.read()
+
+    #display the frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+# When finished or if false release the capture
+cap.release()
+cv2.destroyAllWindows()
