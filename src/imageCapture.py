f32a006037e6f3bcc0df0b64f1cea00f64db2d8d
boyd ndonga <boydbugle@gmail.com>
2018-10-11 14:42:11 +0300

    adjust image frame scale

--- src/imageCapture.py
+++ src/imageCapture.py
@@ -14,7 +14,7 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Detect faces in the image
 faces = faceCascade.detectMultiScale(
     gray,
-    scaleFactor=1.2,
+    scaleFactor=1.1,
     minNeighbors=5,
     minSize=(30, 30)
     #flags = cv2.CV_HAAR_SCALE_IMAGE
