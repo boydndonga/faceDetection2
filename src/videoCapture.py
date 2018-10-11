3ef1c7f6cf89dc97562a9000707fdf5326aa8476
boyd ndonga <boydbugle@gmail.com>
2018-10-11 12:35:39 +0300

    create multiscale for video frame

--- src/videoCapture.py
+++ src/videoCapture.py
@@ -7,6 +7,15 @@ while(True):
     #capture video frame
     ret, frame = cap.read()
 
+    # create gray filter and map rectangle to faces
+    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    print("Found {0} faces!".format(len(faces)))
+    for (x, y, w, h) in faces:
+    	#print(x,y,w,h)
+    	roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end) (xcord_start, xcord_end)
+    	roi_color = frame[y:y+h, x:x+w]
+
     #display the frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
