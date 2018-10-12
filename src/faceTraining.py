56805198f4acb0b6630e2d6afbaa5bc1e51708d4
boyd ndonga <boydbugle@gmail.com>
2018-10-12 23:52:18 +0300

    checkout to new branch

--- src/faceTraining.py
+++ src/faceTraining.py
@@ -16,4 +16,4 @@ for root, dirs, files in os.walk(image_dir):
             label = os.path.basename(os.path.dirname(path)).lower()
             # print(label, path)
             pil_image = Image.open(path).convert("L")  # to grayscale read pillow documentation
-            image_array = np.array(pil_image, "uint8")
+            image_array = np.array(pil_image, "uint8")  # convert image to numpy array values
