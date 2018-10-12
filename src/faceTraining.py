af57976b82b0baf2693f987ef8c890e6798a4713
boyd ndonga <boydbugle@gmail.com>
2018-10-12 23:50:27 +0300

    convert image to numpy array values

--- src/faceTraining.py
+++ src/faceTraining.py
@@ -1,11 +1,19 @@
-import cv2
 import os
+from PIL import Image
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
 
+x_labels = []
+x_train = []
+
 for root, dirs, files in os.walk(image_dir):
     for file in files:
         if file.endswith("png") or file.endswith("jpg") or file.endswith("jpeg"):
+            # get path to images
             path = os.path.join(root, file)
-            print(path)
+            # get path to folders in images which are labels to the respective images
+            label = os.path.basename(os.path.dirname(path)).lower()
+            # print(label, path)
+            pil_image = Image.open(path).convert("L")  # to grayscale read pillow documentation
+            image_array = np.array(pil_image, "uint8")
