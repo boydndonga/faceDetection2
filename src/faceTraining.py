91ac2ce38a03c05b5f6186171fd6f235cfa79b23
boyd ndonga <boydbugle@gmail.com>
2018-10-13 00:08:28 +0300

    create ids to every label

--- src/faceTraining.py
+++ src/faceTraining.py
@@ -6,8 +6,9 @@ image_dir = os.path.join(BASE_DIR, "images")
 
 face_cascade =cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
-
-x_labels = []
+current_id = 0
+label_ids = {}
+y_labels = []
 x_train = []
 
 for root, dirs, files in os.walk(image_dir):
@@ -18,5 +19,18 @@ for root, dirs, files in os.walk(image_dir):
             # get path to folders in images which are labels to the respective images
             label = os.path.basename(os.path.dirname(path)).lower()
             # print(label, path)
+
+            # check if labels have been given ids so as to append to y_labels by id
+            if label not in label_ids:
+                label_ids[label] = current_id
+                current_id += 1
+            id_ = label_ids[label]
+
             pil_image = Image.open(path).convert("L")  # to grayscale read pillow documentation
             image_array = np.array(pil_image, "uint8")  # convert image to numpy array values
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+            for (x, y, w, h) in faces:
+                roi = image_array[y:y + h, x:x + w]
+                x_train.append(roi)
+                y_labels.append(id_)
