f58324be03820f87c8aecc2515f2b85c62b96020
boyd ndonga <boydbugle@gmail.com>
2018-10-12 17:26:16 +0300

    fix bug

---
+++ src/videoCapture2.py
@@ -0,0 +1,42 @@
+import cv2
+import sys
+
+# cascPath = sys.argv[1]
+# faceCascade = cv2.CascadeClassifier(cascPath)
+faceCascade =cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.5,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        print(x,y,w,h)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        img_item = 'myImg.png'
+        img2_item = 'myImg2.png'
+        cv2.imwrite(img_item,roi_gray)
+        cv2.imwrite(img2_item,roi_color)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
