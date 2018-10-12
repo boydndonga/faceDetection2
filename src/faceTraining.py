2aec1816082af0c976c5b41d4e65bf87ea2c74bf
boyd ndonga <boydbugle@gmail.com>
2018-10-12 23:32:39 +0300

    create model to train on recognition

---
+++ src/faceTraining.py
@@ -0,0 +1,11 @@
+import cv2
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(BASE_DIR, "images")
+
+for root, dirs, files in os.walk(image_dir):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpg") or file.endswith("jpeg"):
+            path = os.path.join(root, file)
+            print(path)
