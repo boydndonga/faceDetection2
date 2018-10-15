1dcaa324b1fd8bfb73270befd8141a294c2a24d0
boyd ndonga <boydbugle@gmail.com>
2018-10-15 23:08:49 +0300

    ignore unecessary files

--- src/imageCapture.py
+++ src/imageCapture.py
@@ -1,5 +1,6 @@
 import cv2
 import sys
+import pickle
 
 # Get user supplied values
 imagePath = sys.argv[1]
@@ -12,10 +13,18 @@ cascPath = "../cascades/data/haarcascade_frontalface_default.xml" #sys.argv[2]
 # Create the haar cascade path and link to classifier
 faceCascade = cv2.CascadeClassifier(cascPath)
 
+recognizer = cv2.face.LBPHFaceRecognizer_create()  #face recognizer module
+recognizer.read("./recognizers/face-trainer.yml")
 
 # Read the image
 image = cv2.imread(imagePath)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# create label dictionary from pickle labels
+labels = {"person": 1}
+with open("pickles/labels.pickle", "rb") as f:
+    first_labels = pickle.load(f)
+    labels = { v:k for k,v in first_labels.items()}
 
 # Detect faces in the image
 faces = faceCascade.detectMultiScale(
@@ -30,6 +39,20 @@ print("Found {0} faces!".format(len(faces)))
 
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
+
+    roi_gray = gray[y:y + h, x:x + w]
+    id_, conf = recognizer.predict(roi_gray)
+    if conf >= 45 and conf <= 85:
+        # print(id_)
+        # print(labels[id_])
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_size = 1
+        name = labels[id_]
+        color = (255, 255, 255)
+        stroke = 2
+        cv2.putText(image, name, (x, y), font, font_size, color, stroke, cv2.LINE_AA)
+
+
     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 cv2.imshow("Faces found", image)
